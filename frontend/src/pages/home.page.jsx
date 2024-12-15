25c01128db318103cb6aa8b32b5b3b69d26709dc
Brandon Pyle <mail4brando@gmail.com>
2024-12-15 18:19:37 -0500

    test

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
     return (
         <div>
             <Navbar />
-            <h1 className="text-3xl font-bold text-center mt-10">Site Under Construction</h1>
+            <h1 className="text-3xl font-bold text-center mt-10">Site Under Construction.</h1>
             <Outlet />
         </div>
     )
