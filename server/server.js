b5abf5d7e841566c45d132905c533ea154481421
bpyle02 <mail4brando@gmail.com>
2025-01-25 11:42:38 -0500

    mixed up the server and frontend urls. Hopefully this fixes it

--- server/server.js
+++ server/server.js
@@ -27,13 +27,10 @@ admin.initializeApp({
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
-let PORT = 3173;
+let PORT = 3000;
 
 server.use(express.json());
-server.use("*", cors({
-    origin: "*",
-    optionsSuccessStatus: 200
-}))
+server.use(cors())
 
 mongoose.connect((process.env.DB_LOCATION), {
     autoIndex: true
