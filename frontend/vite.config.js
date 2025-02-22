e6e62df4ee2883f0c14e34ef1004d872a5704041
Brandon Pyle <mail4brando@gmail.com>
2025-02-22 10:20:11 -0500

    updated allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    // allowedHosts: ['christisking.info']
+    allowedHosts: ['christisking.info', '10.0.0.16', '10.0.0.16:5137', '10.0.0.16:3173', 'api.christisking.info']
   },
   test: {
     globals: true,
