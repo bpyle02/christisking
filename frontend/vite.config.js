d14056c72288f7b1f15da31e1b4f16377c8bd8e1
Brandon Pyle <mail4brando@gmail.com>
2025-03-09 22:26:56 -0400

    potential fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
     host: true,
     allowedHosts: ['christisking.info', '10.0.0.16', '10.0.0.16:5137', '10.0.0.16:3173', 'api.christisking.info']
